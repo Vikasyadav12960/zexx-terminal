116bccb4b7ff0a4bc6bc25a971da90ef8cf3ce4d
vickyy <rajurajukatta3@gmail.com>
2026-08-14 21:53:06 +0530

    feat: add command engine

---
+++ core/command.py
@@ -0,0 +1,29 @@
+class Command:
+    """Represents a single ZEXX command."""
+
+    def __init__(self, name, description, handler):
+        self.name = name
+        self.description = description
+        self.handler = handler
+
+    def execute(self, args):
+        return self.handler(args)
+
+
+class CommandEngine:
+    """Stores and executes ZEXX commands."""
+
+    def __init__(self):
+        self.commands = {}
+
+    def register(self, command):
+        self.commands[command.name] = command
+
+    def execute(self, name, args=None):
+        if name not in self.commands:
+            return f"Unknown command: {name}"
+
+        return self.commands[name].execute(args or [])
+
+    def list_commands(self):
+        return self.commands.values()
